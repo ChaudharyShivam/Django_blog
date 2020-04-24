6f791ad4610f51581f149792c2d9e70085268063
unknown <you@example.cShivam>
2020-04-24 10:09:26 +0530

    database connectivity and migration

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Article
 
 def article_list(request):
-     return render(request, 'Articles/aticle_list.html')
+     article = Article.objects.all().order_by('date')
+     return render(request, 'Articles/aticle_list.html', {'articles':article})
 
 
 def about(request):
